c5083e00d1f7a3bc08bb1ad2c0990fbfd3ccfe29
Jason Barbee <jbarbee@ENGR-46223-MAC.local>
2023-03-22 11:03:14 -0500

    go

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-if [[ $SHELL == *"zsh"*  ]]; then
+if [[ $SHELL == *"zsh"* ]]; then
     echo "Deploying zsh mode"
-    cp -R ~/dotfiles/.files ~/
-    cp ~/dotfiles/.zshrc ~/.zshrc
-    cp ~/dotfiles/.alias ~/.alias
-    cp ~/dotfiles/.zprofile ~/.zprofile
+    #cp -R ~/.dotfiles/.files ~/
+    #cp ~/.dotfiles/.zshrc ~/.zshrc
+    cp ~/.dotfiles/.alias ~/.alias
+    #cp ~/.dotfiles/.zprofile ~/.zprofile
 elif [[ $SHELL == *"bash"* ]]; then
     echo "Deploying bash mode"
     cp -R ~/.files ../
