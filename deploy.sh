f2e54a45e039b37dce11f9998aad8bad3fca908c
Jason Barbee <jbarbee@ENGR-46223-MAC.local>
2023-03-22 10:51:42 -0500

    first

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+if [[ $SHELL == *"zsh"*  ]]; then
+    echo "Deploying zsh mode"
+    cp -R ~/dotfiles/.files ~/
+    cp ~/dotfiles/.zshrc ~/.zshrc
+    cp ~/dotfiles/.alias ~/.alias
+    cp ~/dotfiles/.zprofile ~/.zprofile
+elif [[ $SHELL == *"bash"* ]]; then
+    echo "Deploying bash mode"
+    cp -R ~/.files ../
+fi
+
+echo "Deploying Brew List"
+cat brew.list | xargs brew install
